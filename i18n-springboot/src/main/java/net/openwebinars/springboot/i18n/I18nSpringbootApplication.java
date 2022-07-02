466cb74466a21c3b169e8c3437085803434e0e40
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-07-02 17:54:03 +0200

    Proyecto base

---
+++ i18n-springboot/src/main/java/net/openwebinars/springboot/i18n/I18nSpringbootApplication.java
@@ -0,0 +1,13 @@
+package net.openwebinars.springboot.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18nSpringbootApplication.class, args);
+	}
+
+}
